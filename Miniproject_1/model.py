0b69db118de828393108cb62d483c0b984a48161
ghaeflig <56826275+ghaeflig@users.noreply.github.com>
2022-04-13 16:17:30 +0200

    load checkpoint, predict, validation

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -71,6 +71,7 @@ class Model(nn.Module):
         self.batch_size = batch_size
         self.num_epoch = num_epoch
         self.depth = len(features)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         
         # Setting number of CONV PER FLOOR
         if conv_by_level == 1: self.conv_func = Single_Conv
@@ -148,15 +149,30 @@ class Model(nn.Module):
         
     def load_pretrained_model(self) -> None:
 		## This loads the parameters saved in bestmodel .pth into the model
-        pass
+        checkpoint = torch.load('others/parameters.pt', map_location=self.device)
+        epoch = checkpoint['epoch']
+        print("=> Loading checkpoint from a trained model at the best epoch {}".format(epoch))
+        self.load_state_dict(checkpoint['model'])
+        if self.optimizer == 'SGD':
+            optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum = 0.9)
+            optimizer.load_state_dict(checkpoint['optimizer'])
 
-    
+
     def train(self, train_input, train_target) -> None:
 		#: train_input : tensor of size (N, C, H, W) containing a noisy version of the images
 		#: train_target : tensor of size (N, C, H, W) containing another noisy version of the same images , which only differs from the input by their noise .
         
         # prepare data for training
         """ ANY PREPROCESSING """
+        split_ratio = 0.9
+        n = train_input.shape[0]
+        shuffled = torch.randperm(train_input.shape[0])
+        train_input = train_input[shuffled]
+        train_target = train_target[shuffled]
+        train_input = train_input[0:int(n*split_ratio),:,:,:]
+        train_target = train_target[0:int(n*split_ratio),:,:,:]
+        val_input = train_input[int(n*split_ratio):n,:,:,:]
+        val_target = train_target[int(n*split_ratio):n,:,:,:]
         
         # prepare model for training
         """problem with function name"""
@@ -166,16 +182,18 @@ class Model(nn.Module):
         
         # keep track of loss
         losses = []
+        val_losses = []
+        best_loss = 1000
         
         for epoch in range(self.num_epoch):
             time_begin = time.time()
             
             # shuffle data to avoid overfitting and create batches
-            shuffled = torch.randperm(train_input.shape[0])
-            input_batches = train_input[shuffled].split(self.batch_size)
-            target_batches = train_target[shuffled].split(self.batch_size)
+            input_batches = train_input.split(self.batch_size)
+            target_batches = train_target.split(self.batch_size)
             
             running_loss = 0
+            val_running_loss = 0
             
             # Train on each batch
             for idx, (input_batch, target_batch) in enumerate(zip(input_batches, target_batches)):
@@ -195,11 +213,45 @@ class Model(nn.Module):
             epoch_loss = running_loss / len(input_batches)
             print ('Epoch [%d/%d], Train loss: %.4f' %(epoch+1, self.num_epoch, epoch_loss))
             losses.append(epoch_loss)
+            
+            """# Validation
+            val_input_batches = val_input.split(self.batch_size)
+            val_target_batches = val_target.split(self.batch_size)
+            self.eval()
+            with torch.no_grad() :
+                for idx, (val_input_batch, val_target_batch) in enumerate(zip(val_input_batches, val_target_batches)):
+                    val_pred = self(val_input_batch)
+                    val_loss = self.loss_func(val_pred, val_target_batch)
+                    val_running_loss += val_loss.item()
+            
+            val_epoch_loss = val_running_loss / len(val_input_batches)
+            print ('Epoch [%d/%d], Validation loss: %.4f' %(epoch+1, self.num_epoch, val_epoch_loss))
+            val_losses.append(val_epoch_loss)
+            
+            
+            if val_epoch_loss < best_loss : # à changer avec la val loss plus tard
+                best_loss = val_epoch_loss
+                best_epoch = epoch
+                print("=> Saving checkpoint")
+                checkpoint = {'epoch': epoch, 'model': self.state_dict(), 'optimizer': optimizer.state_dict()}
+                torch.save(checkpoint, 'others/parameters.pt')
+        
+        print('Training finished with best best results at epoch {} | Validation loss : {} | Training loss :'.format(best_epoch, best_loss, losses[epoch]))"""
         
         return losses
 
+    
+    
     def predict(self, test_input) -> torch.Tensor:
 		#: test_input : tensor of size (N1 , C, H, W) that has to be denoised by the trained the loaded network .
 		#: returns a tensor of the size (N1 , C, H, W)
-        model.eval()
-        pass
+        #self.train(False)
+        self.eval()
+        test_output = torch.empty(test_input.shape)
+        test_batches = test_input.split(self.batch_size) #shuffled or not ?
+        with torch.no_grad() :
+            for b in range(0, test_input.size(0), self.batch_size) :
+                out = self(test_batch)
+                for k in range(self.batch_size) :
+                    test_output[b + k,:,:,:] = out[k,:,:,:]
+        return test_output
