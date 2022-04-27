216d9e6971c13bf4ae56b5a4a2ac250b086ac885
ghaeflig <56826275+ghaeflig@users.noreply.github.com>
2022-04-27 16:44:32 +0200

    helpers plots

--- Miniproject_1/others/helpers.py
+++ Miniproject_1/others/helpers.py
@@ -1,6 +1,7 @@
 import torch
 import os
 import matplotlib.pyplot as plt
+import sys
 
 sys.path.append('../')
 from model import *
@@ -8,20 +9,29 @@ from model import *
 def psnr(denoised , ground_truth):
 	# Peak Signal to Noise Ratio : denoised and ground_truth have range [0, 1]
 	#mse = torch.mean(( denoised - ground_truth ) ** 2)
-    mse = ( denoised - ground_truth ) ** 2
-	return -10 * torch.log10(mse + 10** -8)
+    mse = ((denoised - ground_truth)**2)
+    return -10 * torch.log10(mse + 10**-8)
 
 
-def create_imgs_plot(denoised, ground_truth, idx=[1,6,10]) :
-    # Save a figure of concatenated images of denoised and ground truth whose indices are specified by idx
-    cimgs = []
-    for i in idx : 
-        cimg = torch.cat((denoised[i,:,:,:].permute(1,2,0), ground_truth[i,:,:,:].permute(1,2,0)), axis=1)
-        cimgs.append(cimg)
+def create_imgs_plot(noisy, denoised, ground_truth, idx=[1,6,10]) :
+    # Save a figure of concatenated images of denoised and ground truth whose indices are specified by id     
+        #cimg = torch.cat((noisy[i,:,:,:].permute(1,2,0), denoised[i,:,:,:].permute(1,2,0), ground_truth[i,:,:,:].permute(1,2,0)), axis=1)
+        #cimgs.append(cimg)
         
-    fig, ax = plt.subplots(len(idx), figsize=(15,15))
-    for j in range(len(idx)) :
-        ax[j].imshow(cimgs[j])
+    fig, ax = plt.subplots(len(idx), 3, figsize=(15,15))
+    for j, i in enumerate(idx) :
+            ax[j, 0].imshow(noisy[i,:,:,:].permute(1,2,0))
+            ax[j, 0].axis('off')
+            ax[j, 1].imshow(denoised[i,:,:,:].permute(1,2,0))
+            ax[j, 1].axis('off')
+            ax[j, 2].imshow(ground_truth[i,:,:,:].permute(1,2,0))
+            ax[j, 2].axis('off')
+            
+    ax[0,0].set_title('Noisy images')
+    ax[0,1].set_title('Denoised images')
+    ax[0,2].set_title('Ground-truth images')
+            
+    plt.suptitle('Noisy, denoised and ground-truth images for some samples of the validation data')
     
     k = 1
     path = f'Concatenated_imgs{k}.png'
@@ -69,17 +79,19 @@ def create_plot_losses():
     
 def create_plot_psnr():
     #sample = 100
-    noisy_test , clean_test = torch.load ('../data/val_data.pkl')
+    noisy_test , clean_test = torch.load ('../../data/val_data.pkl')
     noisy_test, clean_test = noisy_test.float()/255, clean_test.float()/255
     #noisy_test_sample,  clean_test_sample = noisy_test[:sample], clean_test[:sample]
     
+    psnrs = []
     labels = ['baseline', 'w/o batchnorm', 'max pooling', '1-conv layers', 'Adam', 'Adagrad', 'data aug.', 'dropout: 0.5', 'more features']
     for i, label in zip(range(1,10,1), labels):
         os.chdir(path='others/run{}'.format(i))
-        current_model = Model.to("cuda" if torch.cuda.is_available() else "cpu")
+        current_model = Model().to("cuda" if torch.cuda.is_available() else "cpu")
         current_model.load_pretrained_model()
         pred_test = current_model.predict(noisy_test)
-        metric = psnr(pred_test)
-        plt.boxplot(metric)
+        metric = psnr(pred_test, clean_test)
+        psnrs.append(metric)
         
+    plt.boxplot(psnrs, labels=labels) 
     plt.show()
