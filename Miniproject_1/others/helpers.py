e46cb7381b31d2420d29792abc827f53423b1ad9
ghaeflig <56826275+ghaeflig@users.noreply.github.com>
2022-04-27 15:36:17 +0200

    helpers

--- Miniproject_1/others/helpers.py
+++ Miniproject_1/others/helpers.py
@@ -1,10 +1,12 @@
 import torch
 import os
 import matplotlib.pyplot as plt
+from model import Model
 
 def psnr(denoised , ground_truth):
 	# Peak Signal to Noise Ratio : denoised and ground_truth have range [0, 1]
-	mse = torch.mean(( denoised - ground_truth ) ** 2)
+	#mse = torch.mean(( denoised - ground_truth ) ** 2)
+    mse = ( denoised - ground_truth ) ** 2
 	return -10 * torch.log10(mse + 10** -8)
 
 
@@ -64,3 +66,18 @@ def create_plot_losses():
 
     
 def create_plot_psnr():
+    #sample = 100
+    noisy_test , clean_test = torch.load ('../data/val_data.pkl')
+    noisy_test, clean_test = noisy_test.float()/255, clean_test.float()/255
+    #noisy_test_sample,  clean_test_sample = noisy_test[:sample], clean_test[:sample]
+    
+    labels = ['baseline', 'w/o batchnorm', 'max pooling', '1-conv layers', 'Adam', 'Adagrad', 'data aug.', 'dropout: 0.5', 'more features']
+    for i, label in zip(range(1,10,1), labels):
+        os.chdir(path='others/run{}'.format(i))
+        current_model = Model.to("cuda" if torch.cuda.is_available() else "cpu")
+        current_model.load_pretrained_model()
+        pred_test = current_model.predict(noisy_test)
+        metric = psnr(pred_test)
+        plt.boxplot(metric)
+        
+    plt.show()
