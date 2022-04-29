f6399d0237d99b64705f611aab310df2b84a74c2
loconsta <loris.constantin@epfl.ch>
2022-04-29 10:41:58 +0200

    added script id to the parameters

--- Miniproject_1/others/plot_performances.py
+++ Miniproject_1/others/plot_performances.py
@@ -4,11 +4,12 @@ import sys
 
 _, script_id = sys.argv
 
-if script_id = 1:
+script_id = int(script_id)
+if script_id == 0:
     labels = ['baseline', 'w/o batchnorm', 'max pooling', '1-conv layers', 'Adam', 'Adagrad', 'data aug.', 'dropout: 0.5', 'more features']
-elif script_id = 2:
+elif script_id == 1:
     labels = ['baseline', 'max pooling', 'data aug.', 'feat. 1', 'feat. 2', 'feat. 3', 'feat. 4', 'feat. 5']
     
     
-create_plot_losses(labels)
-create_plot_psnr(labels)
+create_plot_losses(script_id, labels)
+create_plot_psnr(script_id, labels)
